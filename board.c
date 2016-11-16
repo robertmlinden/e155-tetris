067b8d5b62d508b970cb98d9fb674c79498f8ae7
Robert Linden <rlinden@g.hmc.edu>
2016-11-16 06:10:50 +0000

    Got rid of most erors?

--- board.c
+++ board.c
@@ -1,37 +1,8 @@
 #include <time.h>
 #include <stdlib.h>
-
-int BOARD_WIDTH = 12;
-int BOARD_HEIGHT = 22;
-
-enum PieceShape {
-	S,
-	Z,
-	I,
-	O,
-	T,
-	L,
-	J
-};
-
-typedef int bool;
-#define true 1
-#define false 0
-
-// (0, 0) is top-left, (NUM_ROWS, 0) is bottom left, (0, NUM_COLS) is top right
-// row number corresponds to first entry corresponds to y-value
-// col number corresponds to second entry corresponds to x-value
-// Therefore, board is indexed (y, x)
-// char board[BOARD_WIDTH][BOARD_HEIGHT];
-
-typedef struct {
-	PieceShape pieceShape;
-	int rotation;
-	
-	// r and c are the location of the top left tile of the 4x4 piece representation
-	int r;
-	int c;
-} FallingPiece;
+#include <stdio.h>
+#include <string.h>
+#include "tetrislib.h"
 
 
 // Rotate
@@ -44,9 +15,10 @@ typedef struct {
 // floating piece on the bottom?
 // Another button to just lead-drop the piece?
 
-void initBoard(char[][] board) {
-	for(int r = 0; r < BOARD_HEIGHT; r++) {
-		for(int c = 0; c < BOARD_WIDTH; c++) {
+void initBoard(char board[BOARD_WIDTH][BOARD_HEIGHT]) {
+	int r, c;
+	for(r = 0; r < BOARD_HEIGHT; r++) {
+		for(c = 0; c < BOARD_WIDTH; c++) {
 			if(r == 0 || r == BOARD_HEIGHT - 1 || c == 0 || c == BOARD_WIDTH - 1) {
 				board[r][c] = '#';
 			}
@@ -56,31 +28,6 @@ void initBoard(char[][] board) {
 		}
 	}
 }
-
-// Niavely stops a piece when it makes contact with one below
-// Returns true if the user can keep PLAYING
-// Returns false if the user lost
-bool tick(FallingPiece* fallingPiece, char[][] board) {
-	fallingPiece -> r = (FallingPiece -> r) + 1;
-
-	for(int r = 0; r < PIECE_BLOCK_SIZE; r++) {
-		for(int c = 0; c < PIECE_BLOCK_SIZE; c++) {
-			int boardRAfterTick = r + fallingPiece->r;
-			int boardCAfterTick = c + fallingPiece->c;
-			if(piece[r][c] != ' ' && board[boardRAfterTick + 1][boardCAfterTick] != ' ') {
-				// The piece needs to stop falling here, so transition to new falling piece
-				bool continueGame = solidifyFallingPiece(fallingPiece, board);
-				if(!continueGame) {
-					return false;
-				}
-				levelElimination(board);
-				newFallingPiece(fallingPiece);
-				return true;
-			}
-		}
-	}
-}
-
 
 // NEED TO ADJUST TO ACCOUNT FOR THE FACT THAT THERE MAY BE PIECES IN THE STARTING SPOT
 void newFallingPiece(FallingPiece* fallingPiece) {
@@ -95,9 +42,14 @@ void newFallingPiece(FallingPiece* fallingPiece) {
 	fallingPiece->c = (BOARD_WIDTH / 2) - 2;
 }
 
-bool solidifyFallingPiece(FallingPiece* fallingPiece, char[][] board) {
-	for(int r = 0; r < PIECE_BLOCK_SIZE; r++) {
-		for(int c = 0; c < PIECE_BLOCK_SIZE; c++) {
+bool solidifyFallingPiece(FallingPiece* fallingPiece, char board[BOARD_WIDTH][BOARD_HEIGHT]) {
+	char piece[PIECE_BLOCK_SIZE][PIECE_BLOCK_SIZE];
+	memcpy(piece, PIECES[fallingPiece->pieceShape][fallingPiece->rotation], 
+		sizeof(char) * PIECE_BLOCK_SIZE * PIECE_BLOCK_SIZE);
+
+	int r, c;
+	for(r = 0; r < PIECE_BLOCK_SIZE; r++) {
+		for(c = 0; c < PIECE_BLOCK_SIZE; c++) {
 			int boardR = r + fallingPiece->r;
 			int boardC = c + fallingPiece->c;
 			if(piece[r][c] != ' ') {
@@ -111,12 +63,44 @@ bool solidifyFallingPiece(FallingPiece* fallingPiece, char[][] board) {
 	return true;
 }
 
-bool move(FallingPiece* fallingPiece, bool moveRight, char[][] baord) {
+// Niavely stops a piece when it makes contact with one below
+// Returns true if the user can keep PLAYING
+// Returns false if the user lost
+bool tick(FallingPiece* fallingPiece, char board[BOARD_WIDTH][BOARD_HEIGHT]) {
+	fallingPiece -> r = (fallingPiece -> r) + 1;
+	char piece[PIECE_BLOCK_SIZE][PIECE_BLOCK_SIZE];
+	memcpy(piece, PIECES[fallingPiece->pieceShape][fallingPiece->rotation], 
+		sizeof(char) * PIECE_BLOCK_SIZE * PIECE_BLOCK_SIZE);
+
+	int r, c;
+	for(r = 0; r < PIECE_BLOCK_SIZE; r++) {
+		for(c = 0; c < PIECE_BLOCK_SIZE; c++) {
+			int boardRAfterTick = r + fallingPiece->r;
+			int boardCAfterTick = c + fallingPiece->c;
+			if(piece[r][c] != ' ' && board[boardRAfterTick + 1][boardCAfterTick] != ' ') {
+				// The piece needs to stop falling here, so transition to new falling piece
+				bool continueGame = solidifyFallingPiece(fallingPiece, board);
+				if(!continueGame) {
+					return false;
+				}
+				// levelElimination(board);
+				newFallingPiece(fallingPiece);
+				return true;
+			}
+		}
+	}
+}
+
+bool move(FallingPiece* fallingPiece, bool moveRight, char board[BOARD_WIDTH][BOARD_HEIGHT]) {
 	int h_offset = moveRight ? 1 : -1;
 	
-	char[][] piece = PIECES[fallingPiece->pieceShape][fallingPiece->rotation];
-	for(int r = 0; r < PIECE_BLOCK_SIZE; r++) {
-		for(int c = 0; c < PIECE_BLOCK_SIZE; c++) {
+	char piece[PIECE_BLOCK_SIZE][PIECE_BLOCK_SIZE];
+	memcpy(piece, PIECES[fallingPiece->pieceShape][fallingPiece->rotation], 
+		sizeof(char) * PIECE_BLOCK_SIZE * PIECE_BLOCK_SIZE);
+
+	int r, c;
+	for(r = 0; r < PIECE_BLOCK_SIZE; r++) {
+		for(c = 0; c < PIECE_BLOCK_SIZE; c++) {
 			int boardRAfterMove = r + fallingPiece->r;
 			int boardCAfterMove = c + fallingPiece->c + h_offset;
 			if(piece[r][c] != ' ' && board[boardRAfterMove][boardCAfterMove] != ' ') {
@@ -126,13 +110,13 @@ bool move(FallingPiece* fallingPiece, bool moveRight, char[][] baord) {
 	}
 
 	// The move was valid!
-	fallingPiece -> c += direction ? 1 : -1;
+	fallingPiece -> c += h_offset;
 	return true;
 }
 
 // STILL NEED TO IMPLEMENT ADJUSTING A PIECE IF SIMPLE ROTATION IS INVALID BUT ADDING A 1 OR 2 SQUARE TRANSLATION WOULD IN FACT MAKE IT VALID
 // VERIFY THIS BY PLAYING SOME TETRIS!
-bool rotate(FallingPiece* fallingPiece, bool rotateClockwise, char[][] board) {
+bool rotate(FallingPiece* fallingPiece, bool rotateClockwise, char board[BOARD_WIDTH][BOARD_HEIGHT]) {
 	// Temporarily rotate the falling piece to see if then new configuration is valid on the board
 	if(rotateClockwise) {
 		fallingPiece -> rotation = (fallingPiece -> rotation + 1) % 4;
@@ -141,9 +125,14 @@ bool rotate(FallingPiece* fallingPiece, bool rotateClockwise, char[][] board) {
 		fallingPiece -> rotation = (fallingPiece -> rotation + 3) % 4;
 	}
 
+	char piece[PIECE_BLOCK_SIZE][PIECE_BLOCK_SIZE];
+	memcpy(piece, PIECES[fallingPiece->pieceShape][fallingPiece->rotation], 
+		sizeof(char) * PIECE_BLOCK_SIZE * PIECE_BLOCK_SIZE);
+
 	// Check to see if configuration is non-overlapping and in-bounds
-	for(int r = 0; r < PIECE_BLOCK_SIZE; r++) {
-		for(int c = 0; c < PIECE_BLOCK_SIZE; c++) {
+	int r, c;
+	for(r = 0; r < PIECE_BLOCK_SIZE; r++) {
+		for(c = 0; c < PIECE_BLOCK_SIZE; c++) {
 			int boardRAfterRotate = r + fallingPiece->r;
 			int boardCAfterRotate = c + fallingPiece->c;
 			if(!isInBounds(boardRAfterRotate, boardCAfterRotate) || (piece[r][c] != ' ' && board[boardRAfterRotate][boardCAfterRotate] != ' ')) {
@@ -169,16 +158,19 @@ bool isInBounds(r, c) {
 	return r <= BOARD_HEIGHT - 2  && c >= 1 && c <= BOARD_HEIGHT - 2;
 }
 
-void displayBoard(FallingPiece* floatingPiece, char[][] board) {
-	fallingPieceRowDisplayBegin = fallingPiece -> r >= 1 ? fallingPiece -> r : 1;
-	fallingPieceRowDisplayEnd = fallingPiece -> r + 3;
-	fallingPieceColDisplayBegin = fallingPiece -> c;
-	fallingPieceColDisplayEnd = fallingPiece -> c + 3;
+void displayBoard(FallingPiece* fallingPiece, char board[BOARD_WIDTH][BOARD_HEIGHT]) {
+	int fallingPieceRowDisplayBegin = fallingPiece -> r >= 1 ? fallingPiece -> r : 1;
+	int fallingPieceRowDisplayEnd = fallingPiece -> r + 3;
+	int fallingPieceColDisplayBegin = fallingPiece -> c;
+	int fallingPieceColDisplayEnd = fallingPiece -> c + 3;
 	
-	char[][] piece = PIECES[fallingPiece->pieceShape][fallingPiece->rotation];
+	char piece[PIECE_BLOCK_SIZE][PIECE_BLOCK_SIZE];
+	memcpy(piece, PIECES[fallingPiece->pieceShape][fallingPiece->rotation], 
+		sizeof(char) * PIECE_BLOCK_SIZE * PIECE_BLOCK_SIZE);
 
-	for(int r = 0; r < BOARD_HEIGHT; r++) {
-		for(int c = 0; c < BOARD_WIDTH; c++) {
+	int r, c;
+	for(r = 0; r < BOARD_HEIGHT; r++) {
+		for(c = 0; c < BOARD_WIDTH; c++) {
 			if(isInSquare(r, c, fallingPieceRowDisplayBegin, fallingPieceRowDisplayEnd,
 								fallingPieceColDisplayBegin, fallingPieceColDisplayEnd)) {
 				char printChar = (board[r][c] == ' ') ? piece[r - fallingPieceRowDisplayBegin][c = fallingPieceColDisplayBegin] :
--- tetris.c
+++ tetris.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "tetrislib.h"
 
 /////////////////////////////////////////////////////////////////////
 // Constants
